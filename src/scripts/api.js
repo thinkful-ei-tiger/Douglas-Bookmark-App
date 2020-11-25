a6c9c77c66b8cdb22b788d8d1507648fbd694cac
Douglas Ede <70340545+Douglas4739@users.noreply.github.com>
2020-11-25 11:59:56 -0800

    Update api.js

--- src/scripts/api.js
+++ src/scripts/api.js
@@ -1,10 +1,8 @@
 /* eslint-disable no-console */
 import store from './store.js';
 
-
-
 function getItems(render) {
-  return fetch('https://thinkful-list-api.herokuapp.com/douglas/bookmarks' )
+  return fetch('https://thinkful-list-api.herokuapp.com/douglas/bookmarks')
 
  
     .then((resp) => {
@@ -26,10 +24,12 @@ const createBookmark = (bookmark) => {
     body: JSON.stringify(bookmark),
   })
     .then(function (resp) {
+      if(!resp.ok){
+        return resp.json().then(evt => Promise.reject(evt))
+      }
       return resp.json();
     })
-    .catch(err => alert('url must follow format: https://www.YOURWEBSITE.com'))
-    
+    .catch(err => alert('url must start with https:// or http://'));
 }; 
 
 
